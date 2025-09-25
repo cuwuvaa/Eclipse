b0afb3e71c8d3f8cd2529c6ae301986d252e3b11
cuwuvaa <ivankolisa@gmail.com>
2025-09-26 02:13:18 +0300

    secured key XD

--- Eclipse/settings.py
+++ Eclipse/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-pk+b@8!rb+j17tqjrhf&q-@!y2s$((*tf(gcaswno!%ui_1ee+'
+#will be regenerated, dont try it :D
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

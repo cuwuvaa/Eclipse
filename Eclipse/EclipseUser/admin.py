9ad239e871f558e850f5c65de25a045abe4bf3d9
cuwuvaa <ivankolisa@gmail.com>
2025-11-08 18:58:18 +0300

    some fixes

--- Eclipse/EclipseUser/admin.py
+++ Eclipse/EclipseUser/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from EclipseUser.models.user import EclipseUser
 
 # Register your models here.
+admin.site.register(EclipseUser)
--- Eclipse/EclipseUser/models/usermanage.py
+++ Eclipse/EclipseUser/models/usermanage.py
@@ -9,9 +9,8 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
